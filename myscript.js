bdf7f13b0dc1f44cf60f15b48f731cd60c67cb77
Jose Ignacio Lomuto <joseignaciolomuto@gmail.com>
2023-05-26 05:48:43 -0300

    primera versión
    calculo completo pero con función prompt()

---
+++ myscript.js
@@ -0,0 +1,39 @@
+
+var precio, desc1, desc2, desc3, cantidad, compra, pagar1, pagar2, pagar3;
+
+precio = 200
+cantidad = parseFloat(prompt("Ingresar la cantidad de tickets que desea comprar"));
+
+compra= precio*cantidad;
+
+desc1= compra*0.80;
+desc2= compra*0.50;
+desc3= compra*0.15;
+
+pagar1=compra-desc1;
+pagar2=compra-desc2;
+pagar3=compra-desc3;
+
+document.write(" | Estudiante? el descuento es: "+desc1);
+document.write(" , El Total a pagar es: "+pagar1);
+
+document.write(" | Trainee? El Descuento es: "+desc2);
+document.write(" , El Total a pagar es: "+pagar2);
+
+document.write(" | Junior? El Descuento es: "+desc3 );
+document.write(" ,  El Total a pagar es: "+pagar3);
+
+
+
+document.getElementById("demo").innerHTML = text;
+
+
+
+
+
+
+
+
+
+
+
