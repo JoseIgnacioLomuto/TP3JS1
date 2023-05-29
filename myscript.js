756dbf756be303bb8d5fdbaacb584b3d5b9c2698
Jose Ignacio Lomuto <joseignaciolomuto@gmail.com>
2023-05-29 15:40:55 -0300

    .-
    A

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
+// primera versión tp 3
 
 var precio, desc1, desc2, desc3, cantidad, compra, pagar1, pagar2, pagar3;
 
@@ -14,18 +15,15 @@ pagar1=compra-desc1;
 pagar2=compra-desc2;
 pagar3=compra-desc3;
 
-document.write(" | Estudiante? el descuento es: "+desc1);
-document.write(" , El Total a pagar es: "+pagar1);
+document.write(" | Estudiante? El descuento es: "+desc1);
+document.write(" , El Total: "+pagar1);
 
 document.write(" | Trainee? El Descuento es: "+desc2);
-document.write(" , El Total a pagar es: "+pagar2);
+document.write(" , El Total: "+pagar2);
 
-document.write(" | Junior? El Descuento es: "+desc3 );
-document.write(" ,  El Total a pagar es: "+pagar3);
+document.write(" | Junior? El Descuento es: "+desc3);
+document.write(" ,  El Total: "+pagar3);
 
-
-
-document.getElementById("demo").innerHTML = text;
 
 
 
